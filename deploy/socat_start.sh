1cb7dafca282fcc1dc0cb0e2b014e9ff4e8d2f8e
Aleksandr Glebov <89081349143@mail.ru>
2024-04-20 15:16:25 -0600

    webhook

--- deploy/socat_start.sh
+++ deploy/socat_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Отправляет HTTP-ответ и запускает webhook.sh при получении запроса
-command="echo -e 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\nReceived request' && bash /home/defendershow/bot/deploy/webhook.sh"
+command="echo -e 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nReceived request' && bash /home/defendershow/bot/deploy/webhook.sh"
 
 # Запускает socat на порту 8888, обрабатывая каждое новое подключение
 socat TCP-LISTEN:8888,reuseaddr,fork SYSTEM:"$command"
