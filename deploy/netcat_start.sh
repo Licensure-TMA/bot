83859d73d1b1693a27a5694482a5a4c231313196
Aleksandr Glebov <89081349143@mail.ru>
2024-04-20 15:10:46 -0600

    webhook

--- deploy/netcat_start.sh
+++ deploy/netcat_start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-while true; do
-    # Ожидание HTTP-запроса и запись его в файл
-    echo -e "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nReceived request" | nc -l -p 8888 > request.txt
+# Отправляет HTTP-ответ и запускает webhook.sh при получении запроса
+command="echo -e 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\nReceived request' && bash /home/defendershow/bot/deploy/webhook.sh"
 
-    # Проверка, был ли файл изменен (получен ли запрос)
-    if [ -s request.txt ]; then
-        # Запуск скрипта webhook.sh, если файл не пустой
-        bash /home/defendershow/bot/deploy/webhook.sh
-    fi
-
-    # Очистка файла для следующего запроса
-    > request.txt
-done
+# Запускает socat на порту 8888, обрабатывая каждое новое подключение
+socat TCP-LISTEN:8888,reuseaddr,fork SYSTEM:"$command"
