89d415716710e891b109bb3df1f97710dc01848e
Aleksandr Glebov <89081349143@mail.ru>
2024-04-20 14:29:57 -0600

    webhook

--- deploy/netcat_start.sh
+++ deploy/netcat_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while true; do
-    nc -l -p 8888 -c "echo -e 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n'; bash /home/defendershow/bot/deploy/webhook.sh" > /dev/null
+    nc -l -p 8888 "echo -e 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n'; bash /home/defendershow/bot/deploy/webhook.sh" > /dev/null
 done
