5bd5799d22d5fa5d141ac4ae72a9fb5901665506
Aleksandr Glebov <89081349143@mail.ru>
2024-04-20 13:29:50 -0600

    webhook

--- deploy/webhook.sh
+++ deploy/webhook.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Путь к скрипту deploy.sh
-DEPLOY_SCRIPT=/path/to/deploy.sh
+DEPLOY_SCRIPT=/home/defendershow/bot/deploy/deploy.sh
 
 # Запустить скрипт развертывания
 $DEPLOY_SCRIPT
 
 # Вернуть ответ веб-хуку
 echo "Deployment triggered successfully."
+
+#netcat config
+#while true; do
+#    echo -e "HTTP/1.1 200 OK\n\n $(bash /home/defendershow/bot/deploy/webhook.sh)" | nc -l -p 8888
+#done
