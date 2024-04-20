931dd9a4a8c1ff402694270b4269b67b2805e204
Aleksandr Glebov <89081349143@mail.ru>
2024-04-20 17:10:10 -0600

    test auto deploy 2

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
         states = {
             START: [CommandHandler('testjopy', test),
                     CommandHandler('testpisi', test),
-                    CommandHandler('test1', test)]
+                    CommandHandler('test2', test)]
         },
         fallbacks = [CommandHandler('cancel', cancel)]
     )
