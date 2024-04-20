2ca1ec4a3437258a187cca7886ba3f15f3c5eba4
Aleksandr Glebov <89081349143@mail.ru>
2024-04-20 14:45:40 -0600

    webhook

--- deploy/netcat_start.sh
+++ deploy/netcat_start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 while true; do
-    nc -l -p 8888 "echo -e 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n'; bash /home/defendershow/bot/deploy/webhook.sh" > /dev/null
+    # Ожидание HTTP-запроса и запись его в файл
+    echo -e "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nReceived request" | nc -l -p 8888 > request.txt
+
+    # Проверка, был ли файл изменен (получен ли запрос)
+    if [ -s request.txt ]; then
+        # Запуск скрипта webhook.sh, если файл не пустой
+        bash /home/defendershow/bot/deploy/webhook.sh
+    fi
+
+    # Очистка файла для следующего запроса
+    > request.txt
 done
