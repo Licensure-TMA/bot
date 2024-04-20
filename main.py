1443310b3525d8a66de38f48b221286c1863ecbd
Aleksandr Glebov <89081349143@mail.ru>
2024-04-20 16:15:13 -0600

    webhook

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
         states = {
             START: [CommandHandler('testjopy', test),
                     CommandHandler('testpisi', test),
-                    CommandHandler('test1', test)]
+                    CommandHandler('test', test)]
         },
         fallbacks = [CommandHandler('cancel', cancel)]
     )
