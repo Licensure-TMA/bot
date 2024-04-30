204bc3c725f45662efedf7153ed84e7278941dee
Aleksandr Glebov <89081349143@mail.ru>
2024-04-29 23:55:49 -0600

    test webhook with https 2

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main():
     start_handler = ConversationHandler(
         entry_points = [CommandHandler('start', start)],
         states = {
-            START: [CommandHandler('test2', test)]
+            START: [CommandHandler('test', test)]
         },
         fallbacks = [CommandHandler('cancel', cancel)]
     )
